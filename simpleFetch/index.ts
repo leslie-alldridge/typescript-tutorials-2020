e8040028ae17fbaaeaeadf96122c3bd45f682ab5
leslie.alldridge <leslie.alldridge@gmail.com>
2020-02-12 21:38:53 +1300

    added interface

--- simpleFetch/index.ts
+++ simpleFetch/index.ts
@@ -2,6 +2,15 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+  const { id, title, completed } = response.data;
+  console.log(id, title, completed);
 });
