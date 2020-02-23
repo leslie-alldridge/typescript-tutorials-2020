2cf399e25f31f4e4e0fc6dacf58e1719cf22e561
leslie.alldridge <leslie.alldridge@gmail.com>
2020-02-23 18:26:27 +1300

    l24

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Revisiting typescript since it has been a lil while!
 `tsc index.ts` to compile into js
 `ts-node index.ts` to compile and run the js version afterwards
 
-L22
+L24
--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,11 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
